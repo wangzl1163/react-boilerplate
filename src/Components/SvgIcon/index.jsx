15567502c37f44bed0ec4949b062f73d64ee00ea
wangzl1163 <wangzl1163@qq.com>
2025-04-27 16:40:31 +0800

    feat: svg icon 组件支持设置宽和高

--- src/Components/SvgIcon/index.jsx
+++ src/Components/SvgIcon/index.jsx
@@ -1,18 +1,18 @@
 function isExternal(path) {
-   return /^(https?:|mailto:|tel:)/.test(path)
+   return /^(https?:|mailto:|tel:)/.test(path);
 }
 
 export default function SvgIcon(props) {
-   const { icon, className } = props
+   const { icon, className, width, height } = props;
    const el = isExternal(icon) ? (
       <div
-         style={{ mask: `url(${icon}) no-repeat 50% 50%`, '-webkit-mask': `url(${icon}) no-repeat 50% 50%` }}
+         style={{ mask: `url(${icon}) no-repeat 50% 50%`, "-webkit-mask": `url(${icon}) no-repeat 50% 50%` }}
          className="svg-external-icon svg-icon"
       ></div>
    ) : (
-      <svg className={className ? 'svg-icon ' + className : 'svg-icon'} aria-hidden="true">
+      <svg className={className ? "svg-icon " + className : "svg-icon"} width={width} height={height} aria-hidden="true">
          <use xlinkHref={`#icon-${icon}`} />
       </svg>
-   )
-   return el
+   );
+   return el;
 }
